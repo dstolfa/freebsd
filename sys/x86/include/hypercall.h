15ced0da75f03b456f7f78bb2eadfd80068f3887
Domagoj Stolfa <domagoj.stolfa@gmail.com>
2016-09-07 13:05:20 +0200

    Fix some of the compilation issues with hypercall.h

--- sys/x86/include/hypercall.h
+++ sys/x86/include/hypercall.h
@@ -1,24 +1,43 @@
-#ifndef _MACHINE_HYPERCALL_H_
-#define _MACHINE_HYPERCALL_H_
+#ifndef _X86_HYPERCALL_H_
+#define _X86_HYPERCALL_H_
 
 #include <sys/cdefs.h>
-#include <machine/cputypes.h>
+#include <sys/types.h>
+
+#include <x86/x86_var.h>
+#include <x86/cputypes.h>
+
 
 #define VMCALL		".byte 0x0f,0x01,0xc1\n"
 #define VMMCALL		".byte 0x0f,0x01,0xd9\n"
 
+typedef u_char bool;
+
 #ifdef __amd64__
+
+/* XXX: Each of these hypercalls can
+ * be rewritten in a better way with
+ * less redundancy
+ */
 
 static __inline long
 hypercall0(unsigned long c)
 {
 	const long nargs = 0;
 	long ret;
-	asm volatile(
-	    (cpu_vendor_id == CPU_VENDOR_INTEL) ? VMCALL : VMMCALL
-	    : "=a"(ret)
-	    : "a"(c), "b"(nargs)
-	    : "memory", "esp");
+	if (cpu_vendor_id == CPU_VENDOR_INTEL) {
+		asm volatile(
+		    VMCALL
+		    : "=a"(ret)
+		    : "a"(c), "b"(nargs)
+		    : "memory", "rsp");
+	} else {
+		asm volatile(
+		    VMMCALL
+		    : "=a"(ret)
+		    : "a"(c), "b"(nargs)
+		    : "memory", "rsp");
+	}
 	return ret;
 }
 
@@ -27,13 +46,23 @@ hypercall1(unsigned long c, unsigned long arg0)
 {
 	const long nargs = 1;
 	long ret;
-	asm volatile(
-	    "push %1\n"
-	    (cpu_vendor_id == CPU_VENDOR_INTEL) ? VMCALL : VMMCALL
-	    "add $8, %%rsp\n"
-	    : "=a"(ret)
-	    : "a"(c), "g"(arg0), "b"(nargs)
-	    : "memory", "esp");
+	if (cpu_vendor_id == CPU_VENDOR_INTEL) {
+		asm volatile(
+		    "push %1\n"
+		    VMCALL
+		    "add $8, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "b"(nargs)
+		    : "memory", "rsp");
+	} else {
+		asm volatile(
+		    "push %1\n"
+		    VMMCALL
+		    "add $8, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "b"(nargs)
+		    : "memory", "rsp");
+	}
 	return ret;
 }
 
@@ -43,14 +72,25 @@ hypercall2(unsigned long c, unsigned long arg0,
 {
 	const long nargs = 2;
 	long ret;
-	asm volatile(
-	    "push %2\n"
-	    "push %1\n"
-	    (cpu_vendor_id == CPU_VENDOR_INTEL) ? VMCALL : VMMCALL
-	    "add $16, %%rsp\n"
-	    : "=a"(ret)
-	    : "a"(c), "g"(arg0), "g"(arg1), "b"(nargs)
-	    : "memory", "esp");
+	if (cpu_vendor_id == CPU_VENDOR_INTEL) {
+		asm volatile(
+		    "push %2\n"
+		    "push %1\n"
+		    VMCALL
+		    "add $16, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "g"(arg1), "b"(nargs)
+		    : "memory", "rsp");
+	} else {
+		asm volatile(
+		    "push %2\n"
+		    "push %1\n"
+		    VMMCALL
+		    "add $16, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "g"(arg1), "b"(nargs)
+		    : "memory", "rsp");
+	}
 	return ret;
 } 
 
@@ -60,15 +100,27 @@ hypercall3(unsigned long c, unsigned long arg0,
 {
 	const long nargs = 3;
 	long ret;
-	asm volatile(
-	    "push %3\n"
-	    "push %2\n"
-	    "push %1\n"
-	    (cpu_vendor_id == CPU_VENDOR_INTEL) ? VMCALL : VMMCALL
-	    "add $24, %%rsp\n"
-	    : "=a"(ret)
-	    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "b"(nargs)
-	    : "memory", "esp");
+	if (cpu_vendor_id == CPU_VENDOR_INTEL) {
+		asm volatile(
+		    "push %3\n"
+		    "push %2\n"
+		    "push %1\n"
+		    VMCALL
+		    "add $24, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "b"(nargs)
+		    : "memory", "rsp");
+	} else {
+		asm volatile(
+		    "push %3\n"
+		    "push %2\n"
+		    "push %1\n"
+		    VMMCALL
+		    "add $24, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "b"(nargs)
+		    : "memory", "rsp");
+	}
 	return ret;
 } 
 
@@ -79,16 +131,29 @@ hypercall4(unsigned long c, unsigned long arg0,
 {
 	const long nargs = 4;
 	long ret;
-	asm volatile(
-	    "push %4\n"
-	    "push %3\n"
-	    "push %2\n"
-	    "push %1\n"
-	    (cpu_vendor_id == CPU_VENDOR_INTEL) ? VMCALL : VMMCALL
-	    "add $32, %%rsp\n"
-	    : "=a"(ret)
-	    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "g"(arg3), "b"(nargs)
-	    : "memory", "esp");
+	if (cpu_vendor_id == CPU_VENDOR_INTEL) {
+		asm volatile(
+		    "push %4\n"
+		    "push %3\n"
+		    "push %2\n"
+		    "push %1\n"
+		    VMCALL
+		    "add $32, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "g"(arg3), "b"(nargs)
+		    : "memory", "rsp");
+	} else {
+		asm volatile(
+		    "push %4\n"
+		    "push %3\n"
+		    "push %2\n"
+		    "push %1\n"
+		    VMMCALL
+		    "add $32, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "g"(arg3), "b"(nargs)
+		    : "memory", "rsp");
+	}
 	return ret;
 } 
 
@@ -99,43 +164,72 @@ hypercall5(unsigned long c, unsigned long arg0,
 {
 	const long nargs = 5;
 	long ret;
-	asm volatile(
-	    "push %5\n"
-	    "push %4\n"
-	    "push %3\n"
-	    "push %2\n"
-	    "push %1\n"
-	    (cpu_vendor_id == CPU_VENDOR_INTEL) ? VMCALL : VMMCALL
-	    "add $40, %%rsp\n"
-	    : "=a"(ret)
-	    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "g"(arg3), "g"(arg4), "b"(nargs)
-	    : "memory", "esp");
+	if (cpu_vendor_id == CPU_VENDOR_INTEL) {
+		asm volatile(
+		    "push %5\n"
+		    "push %4\n"
+		    "push %3\n"
+		    "push %2\n"
+		    "push %1\n"
+		    VMCALL
+		    "add $40, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "g"(arg3), "g"(arg4), "b"(nargs)
+		    : "memory", "rsp");
+	} else {
+		asm volatile(
+		    "push %5\n"
+		    "push %4\n"
+		    "push %3\n"
+		    "push %2\n"
+		    "push %1\n"
+		    VMMCALL
+		    "add $40, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "g"(arg3), "g"(arg4), "b"(nargs)
+		    : "memory", "rsp");
+	}
 	return ret;
 } 
 
 static __inline long
-hypercall6(unsigned long c, unsigned long arg0
+hypercall6(unsigned long c, unsigned long arg0,
     unsigned long arg1, unsigned long arg2,
     unsigned long arg3, unsigned long arg4,
     unsigned long arg5)
 {
 	const long nargs = 6;
 	long ret;
-	asm volatile(
-	    "push %6\n"
-	    "push %5\n"
-	    "push %4\n"
-	    "push %3\n"
-	    "push %2\n"
-	    "push %1\n"
-	    (cpu_vendor_id == CPU_VENDOR_INTEL) ? VMCALL : VMMCALL
-	    "add $48, %%rsp\n"
-	    : "=a"(ret)
-	    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "g"(arg3), "g"(arg4), "g"(arg5), "b"(nargs)
-	    : "memory", "esp");
+	if (cpu_vendor_id == CPU_VENDOR_INTEL) {
+		asm volatile(
+		    "push %6\n"
+		    "push %5\n"
+		    "push %4\n"
+		    "push %3\n"
+		    "push %2\n"
+		    "push %1\n"
+		    VMCALL
+		    "add $48, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "g"(arg3), "g"(arg4), "g"(arg5), "b"(nargs)
+		    : "memory", "rsp");
+	} else {
+		asm volatile(
+		    "push %6\n"
+		    "push %5\n"
+		    "push %4\n"
+		    "push %3\n"
+		    "push %2\n"
+		    "push %1\n"
+		    VMMCALL
+		    "add $48, %%rsp\n"
+		    : "=a"(ret)
+		    : "a"(c), "g"(arg0), "g"(arg1), "g"(arg2), "g"(arg3), "g"(arg4), "g"(arg5), "b"(nargs)
+		    : "memory", "rsp");
+	}
 	return ret;
 }
 
 #endif /* __amd64__ */
 
-#endif /* _MACHINE_HYPERCALL_H_ */
+#endif /* _X86_HYPERCALL_H_ */
