b2b7ec28dbb9cbec1b514cef48395cb236eba190
Domagoj Stolfa <domagoj.stolfa@gmail.com>
2017-03-01 14:19:10 +0100

    Additional code in the host side of virtio

--- usr.sbin/bhyve/pci_virtio_dtrace.c
+++ usr.sbin/bhyve/pci_virtio_dtrace.c
@@ -63,6 +63,14 @@ __FBSDID("$FreeBSD$");
 #define	VTDTR_DEVICE_PROBE_INSTALL	0x05
 #define	VTDTR_DEVICE_PROBE_UNINSTALL	0x06
 
+#define	VTDTR_F_PROBE			0x01
+#define	VTDTR_F_PROV			0x02
+/*
+ * We only support probe operations right now
+ */
+#define	VTDTR_S_HOSTFEATURES		\
+    (VTDTR_F_PROBE)
+
 static int pci_vtdtr_debug;
 #define	DPRINTF(params)		if (pci_vtdtr_debug) printf params
 #define	WPRINTF(params)		printf params
@@ -72,3 +80,109 @@ struct pci_vtdtr_control {
 	uint32_t	 value;
 	char		*info;
 } __attribute__((packed));
+
+struct pci_vtdtr_softc {
+	struct virtio_softc		vsd_vs;
+	struct vqueue_info		vsd_ctrl_rx;
+	struct vqueue_info		vsd_ctrl_tx;
+	pthread_mutex_t			vsd_mtx;
+	uint64_t			vsd_features;
+	int				vsd_ready;
+};
+
+static struct virtio_consts vtdtr_vi_consts = {
+	"vtdtr",			/* name */
+	VTDTR_MAXQ,			/* maximum virtqueues */
+	0,				/* config reg size */
+	pci_vtdtr_reset,		/* reset */
+	NULL,				/* device-wide qnotify */
+	NULL,				/* read virtio config */
+	NULL,				/* write virtio config */
+	pci_vtdtr_neg_features,		/* apply negotiated features */
+	VTDTR_S_HOSTFEATURES		/* features */
+};
+
+static void
+pci_vtdtr_reset(void *vsc)
+{
+	struct pci_vtdtr_softc *sc;
+
+	sc = vsc;
+
+	DPRINTF(("vtdtr: device reset requested!\n"));
+	vi_reset_dev(&sc->vsd_vs);
+}
+
+static __inline void
+pci_vtdtr_neg_features(void *vsc, uint64_t negotiated_features)
+{
+	struct pci_vtdtr_softc *sc = vsc;
+	sc->vsd_features = negotiated_features;
+}
+
+static void
+pci_vtdtr_control_tx(struct pci_vtdtr_softc *sc, void *arg, struct iovec *iov,
+    int niov)
+{
+	struct pci_vtdtr_control  resp;
+	struct pci_vtdtr_control *ctrl;
+
+	assert(niov == 1);
+
+	ctrl = (struct pci_vtdtr_control *)iov->iov_base;
+
+	switch (ctrl->event) {
+	case VTDTR_DEVICE_READY:
+		sc->vsd_ready = 1;
+		break;
+	case VTDTR_DEVICE_REGISTER:
+	case VTDTR_DEVICE_UNREGISTER:
+	case VTDTR_DEVICE_DESTROY:
+		if (sc->vtd_features & VTDTR_F_PROV)
+			pci_vtdtr_process_prov_evt(sc, ctrl);
+		break;
+	case VTDTR_DEVICE_PROBE_CREATE:
+	case VTDTR_DEVICE_PROBE_INSTALL:
+	case VTDTR_DEVICE_PROBE_UNINSTALL:
+		if (sc->vtd_features & VTDTR_F_PROBE)
+			pci_vtdtr_process_probe_evt(sc, ctrl);
+		break;
+	}
+}
+
+static void
+pci_vtdtr_control_send(struct pci_vtdtr_softc *sc,
+    struct pci_vtdtr_control *ctrl)
+{
+	/*
+	 * XXX: This might not be very correct
+	 */
+	struct vqueue_info *vq;
+	struct iovec iov;
+	uint32_t len;
+	uint16_t idx;
+	int n;
+
+	vq = sc->vsd_ctrl_tx;
+
+	if (!vq_has_descs(vq))
+		return;
+
+	n = vq_getchain(vq, &idx, &iov, 1, NULL);
+
+	assert (n == 1);
+
+	memcpy(iov.iov_base, ctrl, sizeof(struct pci_vtdtr_control));
+	len = iov.iov_len;
+
+	vq_relchain(vq, idx, len);
+	vq_endchains(vq, 1);
+}
+
+struct pci_devemu pci_de_vdtr = {
+	.pe_emu		= "virtio-dtrace",
+	.pe_init	= pci_vtdtr_init,
+	.pe_barwrite	= vi_pci_write,
+	.pe_barread	= vi_pci_read
+};
+PCI_EMUL_SET(pci_de_vdtr);
