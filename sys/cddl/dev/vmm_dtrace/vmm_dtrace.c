8865de516c9e410d7f0129bf969f749f7fcc2c63
Domagoj Stolfa <domagoj.stolfa@gmail.com>
2017-06-06 01:03:07 +0200

    Licenses.

--- sys/cddl/dev/vmm_dtrace/vmm_dtrace.c
+++ sys/cddl/dev/vmm_dtrace/vmm_dtrace.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2016 Domagoj Stolfa <domagoj.stolfa@gmail.com>
+ * Copyright (c) 2017 Domagoj Stolfa <domagoj.stolfa@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
